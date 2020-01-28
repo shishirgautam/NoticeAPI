2ed9187d9dde604a260e373b44053353473bb99d
shishirgautam <gautamshishir2@gmail.com>
2020-01-29 00:04:13 +0545

    [#4]update validation and user authentication

--- routes/auths.js
+++ routes/auths.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const {registerValidation} = require('../validation');
+//const {loginValidation} = require('../validation');
 
-router.post('/', async (req,res) => {
+
+router.post('/register', async (req, res) => {
+
+  //LESTS VALIDATE THE DATA BEFORE WE A USER
+    const { error } = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+
+  // Checking if the user is already in the database
+      const emailExits = await User.findOne({email: req.body.email});
+      if(emailExits) return res.status(400).send('Email is already exits');
+  
+  //Hash passwords
+  const salt = await bcrypt.getSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+
+  
+  
+  //Create a user
   const user = new User({
       username: req.body.username,
       email: req.body.email,
@@ -11,10 +33,27 @@ router.post('/', async (req,res) => {
   }); 
   try{
          const savedUser = await user.save()
-          res.send(savedUser);
+          res.send({user: user._id });
   }catch(err){
-      res.status(400).send(err)
-  }
+      res.status(400).send(err);
+      }
 
-});
+    });
+
+
+  //LOGIN
+  router.post('/login', async (req, res) => {
+
+    //LESTS VALIDATE THE DATA BEFORE WE A USER
+      const { error } = loginValidation(req.body);
+      if(error) return res.status(400).send(error.details[0].message);
+  
+     // Checking if the emailexits
+      const user = await User.findOne({email: req.body.email});
+      if(!user) return res.status(400).send('Email or Password is wrong');
+  });
+
+      //Passsword is correct
+  
+  
   module.exports = router;
