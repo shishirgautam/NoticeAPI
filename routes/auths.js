16015c38a6b65aedd585cab964d7ccac80ab0a6f
shishirgautam <gautamshishir2@gmail.com>
2020-01-29 13:00:25 +0545

    package json

---
+++ routes/auths.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {registerValidation,loginValidation} = require('../validation');
+
+
+
+router.post('/register', async (req, res) => {
+
+  //LESTS VALIDATE THE DATA BEFORE WE A USER
+    const { error } = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+
+  // Checking if the user is already in the database
+      const emailExits = await User.findOne({email: req.body.email});
+      if(emailExits) return res.status(400).send('Email is already exits');
+  
+  //Hash passwords
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+
+  
+  
+  //Create a user
+  const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPassword,
+      image: req.body.image
+  }); 
+  try{
+         const savedUser = await user.save()
+          res.send({user: user._id });
+  }catch(err){
+      res.status(400).send(err);
+      }
+
+    });
+
+
+  //LOGIN
+  router.post('/login', async (req, res) => {
+
+    //LESTS VALIDATE THE DATA BEFORE WE A USER
+      const { error } = loginValidation(req.body);
+      if(error) return res.status(400).send(error.details[0].message);
+  
+     // Checking if the emailexits
+      const user = await User.findOne({email: req.body.email  });
+      if(!user) return res.status(400).send('Email is not found');
+
+
+      //Passsword is correct
+      const validPass = await bcrypt.compare(req.body.password, user.password);
+      if(!validPass)  return res.status(400).send('invalid password')
+
+    //Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.SECRET);
+    res.header('auth-token', token).send(token);
+
+      res.send('Logged in');
+  
+    });
+  module.exports = router;
