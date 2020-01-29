ad201a59bbc801d967a7ef452350a9c4fc1d9ef1
shishirgautam <gautamshishir2@gmail.com>
2020-01-29 11:51:45 +0545

    [#3]error login validation

--- routes/auths.js
+++ routes/auths.js
@@ -30,7 +30,7 @@ router.post('/register', async (req, res) => {
   }); 
   try{
          const savedUser = await user.save()
-          res.send(savedUser);
+          res.send({user: user._id});
   }catch(err){
       res.status(400).send(err);
       }
